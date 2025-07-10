b258c5f4398feff8e13ce80fa97bc34af52c28b6
TsZone0 <shivamkumar.tit@gmail.com>
2025-07-10 20:54:55 +0530

    basics

---
+++ a_first.java
@@ -0,0 +1,6 @@
+class a_first{
+    public static void main(String args[]){
+        System.out.println("hello");
+        System.out.println("hello\n"+"shivam");
+    }
+}
---
+++ b_variable.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class b_variable {
+    public static void main(String args[]){
+        int a = 22;
+        int b = 34;
+        int sum = a+b;
+        System.out.println(sum);
+
+        //input
+
+        Scanner sc = new Scanner(System.in);
+        String Name = sc.next();
+        System.out.println("Hello "+Name);
+
+        //nextLine()
+        //nextInt()
+        //nextfloat()
+
+    }
+}
