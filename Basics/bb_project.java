60c4f320df0b9173af98e009162cb087bc8f8d09
TsZone0 <shivamkumar.tit@gmail.com>
2025-07-11 08:40:09 +0530

    Add files via upload

---
+++ Basics/bb_project.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class bb_project {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your First Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter Your Second Number: ");
+        int num2 = sc.nextInt();
+
+        int total = num1+num2;
+        System.out.println("Your Total is = "+ total);
+    }
+}
