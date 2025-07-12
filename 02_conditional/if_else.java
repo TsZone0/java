13ced655fd021bebe316a1b866b0840420bd2c62
TsZone0 <shivamkumar.tit@gmail.com>
2025-07-12 08:16:57 +0530

    Add files via upload

---
+++ 02_conditional/if_else.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class if_else {
+    public static void main(String[] args) {
+int age = 30;
+if(age > 18) {
+   System.out.println("This is an adult");
+} else {
+   System.out.println("This is not an adult");
+}
+
+    }
+}
