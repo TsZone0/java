b7fe3783b486d1e69a8dcaff15201e21c1f6c8f0
TsZone0 <shivamkumar.tit@gmail.com>
2025-07-13 07:20:34 +0530

    Add files via upload

---
+++ 03_Loops/do_loop.java
@@ -0,0 +1,12 @@
+public class do_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        do{
+            i++;
+            System.out.println(i);
+
+
+        }while(i<10);
+    }
+}
---
+++ 03_Loops/while_loop.java
@@ -0,0 +1,9 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<10){
+            i++;
+            System.out.println("hello shivam");
+        }
+    }
+}
