77668f0861f4ca456f43ec07645c90d605ba9efd
Shivam Kumar <shivamkumar.tit@gmail.com>
2025-07-15 06:59:52 +0530

    Add files via upload

---
+++ 05_array/array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class array {
+    public static void main(String[] args) {
+        
+    
+    int[] array = new int[5];
+    array[0]= 45;
+    array[1]= 45;
+    array[2]= 45;
+    array[3]= 45;
+    array[4]= 45;
+
+    int array2[] = {23,23,25,65};
+
+    System.out.println(array[1]);
+    System.out.println(array2[2]);
+}
+
+}
