406c82745fbcc59f5caea432f207c2e86b1d13af
Shivam Kumar <shivamkumar.tit@gmail.com>
2025-07-16 07:09:27 +0530

    Add files via upload

---
+++ 06_String/a_string.java
@@ -0,0 +1,24 @@
+
+
+public class a_string {
+    public static void main(String[] args) {
+        String name = "Shivam";
+        String fullname = "Shivam kumar";
+        System.out.println(fullname.length());
+
+        // for caracter
+        System.out.println(name.charAt(5));
+
+        //compare
+
+        String same = "same";
+        String same2 = "same";
+        
+        if(same.compareTo(same2)==0){
+            System.out.println("yes");
+
+            // string is inmutable  (no edit and delete )
+
+        }
+}
+}
