02cde0162c5336afdfa56d441e2a5d15f67f5f45
Shivam Kumar <shivamkumar.tit@gmail.com>
2025-08-25 06:35:28 +0530

    Add files via upload

---
+++ 09_recertion/shorted.java
@@ -0,0 +1,18 @@
+public class shorted {
+    public static boolean isshorted(int arr[], int idx) {
+        if(idx==arr.length-1){
+            return true;
+
+        }
+        if(arr[idx]<arr[idx+1]){
+            return isshorted(arr, idx+1);
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,3,4,4};
+        System.out.println(isshorted(arr, 0));
+    }
+}
