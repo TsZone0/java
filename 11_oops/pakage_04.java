d62196885f17e0afc2308f03025629c9b44ef44e
Shivam Kumar <shivamkumar.tit@gmail.com>
2025-12-05 08:19:07 +0530

    Add files via upload

---
+++ 11_oops/pakage_04.java
@@ -0,0 +1,15 @@
+// pakage ka use karke ham kisi dusre file ke proparty ko access kar skate hai
+//build in pakage
+//coustome pakage
+
+import java.util.*;
+import polymorphism_02;
+
+
+class bankacc{
+    String name;
+    int acc_no;
+}
+public class pakage_04 {
+    
+}
