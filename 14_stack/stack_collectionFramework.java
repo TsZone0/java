aad99e8f184f7d01a7cb341bb53e2ad1d30d99e6
Shivam Kumar <shivamkumar.tit@gmail.com>
2026-01-02 00:36:25 +0530

    Add files via upload

---
+++ 14_stack/stack_collectionFramework.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class stack_collectionFramework{
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<>();
+        s.push(5);
+        s.push(3);
+        s.push(7);
+        s.push(2);
+        s.push(1);
+        
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
